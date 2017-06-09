3881c01466f7fd0f13318c7a138c49b89db85b0a
ivansanchezvera <ivansanchez_1988@hotmail.com>
2017-06-09 18:11:45 -0500

    Google Cloud Image Storage: Guardado y leido basico de image hacia y desde el storage

---
+++ src/cloud/GoogleCloudStorageWorker.java
@@ -0,0 +1,64 @@
+package cloud;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import com.google.cloud.ReadChannel;
+import com.google.cloud.storage.Blob;
+//Imports the Google Cloud client library
+import com.google.cloud.storage.Bucket;
+import com.google.cloud.storage.Bucket.BlobTargetOption;
+import com.google.cloud.storage.BucketInfo;
+import com.google.cloud.storage.Storage;
+import com.google.cloud.storage.StorageOptions;
+
+public class GoogleCloudStorageWorker {
+
+	private String bucketName = "guiamovilse_recursos_storage";
+	public void saveImage(String blobId, byte[] content)
+	{
+	    // Instantiates a client
+	    Storage storage = (Storage) StorageOptions.getDefaultInstance().getService();
+
+	    
+	    // The name for the new bucket
+	    
+
+	    //
+	    // Creates the new bucket
+	    //Bucket bucket = storage.create(BucketInfo.of(bucketName));
+	    Bucket bucket = storage. get(bucketName);
+	    
+	    Blob blob = bucket.create(blobId, content, BlobTargetOption.doesNotExist());
+	    System.out.println(blob.getSelfLink());
+	    System.out.printf("Bucket %s created.%n", bucket.getName());
+	}
+	
+	//Tomado de https://stackoverflow.com/questions/25141998/how-to-download-a-file-from-google-cloud-storage-with-java
+	public void readImage(String blobId)
+	{
+		/*Storage storage = StorageOptions.newBuilder()
+	            .setProjectId(projectId)
+	            .setCredentials(GoogleCredentials.fromStream(new FileInputStream(serviceAccountJSON)))
+	            .build()
+	            .getService();
+	            */
+		Storage storage = (Storage) StorageOptions.getDefaultInstance().getService();
+		Blob blob = storage.get(bucketName, blobId);
+		ReadChannel readChannel = blob.reader();
+		FileOutputStream fileOuputStream;
+		try {
+			fileOuputStream = new FileOutputStream("imagenTest.jpg");
+			fileOuputStream.getChannel().transferFrom(readChannel, 0, Long.MAX_VALUE);
+			fileOuputStream.close();
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+	}
+}
